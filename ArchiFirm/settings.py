9b8a3546cf6522564bde019f67dcb7c872eda672
Abhishek <singh.abhishek151019@gmail.com>
2022-08-06 17:35:18 +0530

    static

--- ArchiFirm/settings.py
+++ ArchiFirm/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wf)$-$6=)gyplezi811p+g=@+bncth!t&eabvs@9nnx0g&d4v3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['build-easy-associates.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
