e9b58c677c427e4be447ae29f0755a9e74680f24
Abhishek Kumar Singh <singh.abhishek151019@gmail.com>
2024-02-24 13:47:29 +0530

    final commit x

--- ArchiFirm/settings.py
+++ ArchiFirm/settings.py
@@ -90,12 +90,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
